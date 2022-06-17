bd4ae4236ec29d18909f7691a42a267cf00ce49d
Brandon Lachterman <blachterman@darkshield.com>
2022-06-16 22:49:32 -0400

    Finally installed the extension correctly

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    
+namespace HeyEarth
+{
+    
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Earth");
+            
+
+        }
+
+    }
+
+
+
+
+}
